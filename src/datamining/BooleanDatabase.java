848b844ab7e4741ec03c24441e83605712632a35
jodyange <67447424+jodyange@users.noreply.github.com>
2025-11-04 10:41:16 +0100

    Add files via upload

---
+++ src/datamining/BooleanDatabase.java
@@ -0,0 +1,23 @@
+package datamining;
+
+import java.util.Set;
+import modelling.BooleanVariable;
+
+public class BooleanDatabase {
+    private Set<BooleanVariable> items;
+
+    public BooleanDatabase(Set<BooleanVariable> items) {
+        this.items = items;
+    }
+
+    public void add(Set<BooleanVariable>){
+
+
+    }
+
+    
+
+
+    public Set<BooleanVariable> getItems() {
+        return items;
+    }}
